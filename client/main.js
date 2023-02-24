69de29bc7921c0a37673b6f7922b0f53a25cdc58
yangshaotong <yangshaotong.baidu.com>
2023-02-24 19:48:03 +0800

    feat: 仿chatGPT UI

--- client/main.js
+++ client/main.js
@@ -52,6 +52,14 @@ function chatStripe(isAi, value, uniqueId) {
                         />
                     </div>
                     <div class='message' id='${uniqueId}'>${value}</div>
+                    ${
+                        isAi ? `
+                            <span class='ai_btn'>
+                                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+                                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
+                            </span>
+                        ` : ''
+                    }
                 </div>
             </div>
         `
@@ -78,7 +86,7 @@ async function handleSubmit(e) {
     loader(messageDiv);
 
     // 请求服务
-    const response = await fetch('https://aichatbot-rriv.onrender.com', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -103,8 +111,42 @@ async function handleSubmit(e) {
 }
 
 form.addEventListener('submit', handleSubmit);
-// form.addEventListener('keyup', (e) => {
-//     if (e.code === 'Enter') {
-//         handleSubmit(e);
-//     }
-// });
+form.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') {
+        handleSubmit(e);
+    }
+});
+
+// 自适应textarea
+// 最小高度
+let minRows = 1;
+// 最大高度，超过则出现滚动条
+let maxRows = 8;
+function autoResize({ target: t }) {
+    console.log('t: ', t);
+    if (t.scrollTop == 0) t.scrollTop = 1;
+    while (t.scrollTop == 0) {
+        if (t.rows > minRows)
+            t.rows--;
+        else
+            break;
+        t.scrollTop = 1;
+        if (t.rows < maxRows)
+            t.style.overflowY = "hidden";
+        if (t.scrollTop > 0) {
+            t.rows++;
+            break;
+        }
+    }
+    while (t.scrollTop > 0) {
+        if (t.rows < maxRows) {
+            t.rows++;
+            if (t.scrollTop == 0) t.scrollTop = 1;
+        }
+        else {
+            t.style.overflowY = "auto";
+            break;
+        }
+    }
+}
+document.querySelector('textarea').addEventListener('keyup', autoResize)
